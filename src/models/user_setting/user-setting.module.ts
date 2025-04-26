50cb112b8df7fab1ecbf8087fed50aeacf43de07
Idan17 <idanbartov@gmail.com>
2025-04-26 15:43:15 +0300

    feat: user settings dto

--- src/models/user_setting/user-setting.module.ts
+++ src/models/user_setting/user-setting.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { UserSettingsController } from "./user-settings.controller";
 import { UserSettingsService } from "./user-settings.service";
-import { PrismaModule } from "../prisma/prisma.module";
+import { PrismaModule } from "database/prisma.module";
 
 @Module({
   imports: [PrismaModule],
