c04c2af1fda7d6aad9a8db26ee0df59293f65d94
mudachii <emudahi@cs.colman.ac.il>
2025-04-27 22:40:24 +0300

    feat: add badges service and DTO for badge management

---
+++ src/models/badge/badges.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { badges, Prisma } from '@prisma/client';
+import { PrismaService } from 'database/prisma.service';
+
+@Injectable()
+export class BadgesService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<badges[]> {
+    return this.prisma.badges.findMany();
+  }
+
+  async findOne(id: number): Promise<badges | null> {
+    return this.prisma.badges.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(data: Prisma.badgesCreateInput): Promise<badges> {
+    return this.prisma.badges.create({
+      data,
+    });
+  }
+
+  async update(id: number, data: Prisma.badgesUpdateInput): Promise<badges> {
+    return this.prisma.badges.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number): Promise<badges> {
+    return this.prisma.badges.delete({
+      where: { id },
+    });
+  }
+}
