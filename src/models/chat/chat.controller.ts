d1fa26e5040e2a35ce884ee74039c44f6ec8e626
mudachii <emudahi@cs.colman.ac.il>
2025-05-12 20:46:08 +0300

    refactor: simplify sendMessage method in ChatController by removing error handling

--- src/models/chat/chat.controller.ts
+++ src/models/chat/chat.controller.ts
@@ -9,17 +9,6 @@ export class ChatController {
 
   @Post()
   async sendMessage(@Body() messageDto: MessageRequestDto): Promise<MessageResponseDto> {
-    try {
-      if (!messageDto.message || typeof messageDto.message !== 'string') {
-        throw new HttpException('Message is required and must be a string', HttpStatus.BAD_REQUEST);
-      }
-      
-      return await this.chatService.processMessage(messageDto);
-    } catch (error) {
-      if (error instanceof HttpException) {
-        throw error;
-      }
-      throw new HttpException('Failed to process message', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return await this.chatService.processMessage(messageDto);
   }
 }
