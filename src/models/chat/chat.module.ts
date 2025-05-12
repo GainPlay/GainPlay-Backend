5d57b9890d00aac0b6d0508983bd988ed2d7c57e
mudachii <emudahi@cs.colman.ac.il>
2025-05-12 20:02:17 +0300

    feat: enhance ChatModule by adding GeminiService and UserSettingsModule imports

--- src/models/chat/chat.module.ts
+++ src/models/chat/chat.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { GeminiService } from '@/models/chat/gemini.service';
+import { UserSettingsModule } from '@/models/user_setting/user-settings.module';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
-  exports: [ChatService],
+  providers: [ChatService,GeminiService],
+    exports: [ChatService],
+    imports: [UserSettingsModule]
 })
 export class ChatModule {}
